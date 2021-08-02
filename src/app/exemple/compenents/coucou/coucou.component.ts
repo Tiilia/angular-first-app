591da552b30956f955c0bc0c9520f7e997881445
Tiilia <dosimontp@gmail.com>
2021-08-02 13:28:19 +0200

    coucou.compenent.ts onInit et onDestroy

--- src/app/exemple/compenents/coucou/coucou.component.ts
+++ src/app/exemple/compenents/coucou/coucou.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-coucou',
   templateUrl: './coucou.component.html',
   styleUrls: ['./coucou.component.scss', './coucou.p.compenent.scss']
 })
-export class CoucouComponent implements OnInit {
+export class CoucouComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor() {
+    console.warn('Je me consutruit');
+
+  }
+  ngOnDestroy(): void {
+    console.error('Je meurs...');
+  }
 
   ngOnInit(): void {
+    console.log("Youpie je suis en vie!");
+
   }
 
 }
