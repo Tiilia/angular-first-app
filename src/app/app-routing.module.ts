4be2ba51f18d2b036e017fe1c45a51ed8e0cb0fe
Tiilia <dosimontp@gmail.com>
2021-08-02 14:00:32 +0200

    base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { CoucouComponent } from './exemple/compenents/coucou/coucou.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "home", component: HomeComponent },
+  { path: 'coucou', component: CoucouComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
